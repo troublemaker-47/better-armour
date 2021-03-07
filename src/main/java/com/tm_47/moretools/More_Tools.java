1a32188c937054fdf45611f43e940219d2ea2571
troublemaker-47 <80205319+troublemaker-47@users.noreply.github.com>
2021-03-07 19:17:55 +0200

    Update More_Tools.java

--- src/main/java/com/tm_47/moretools/More_Tools.java
+++ src/main/java/com/tm_47/moretools/More_Tools.java
@@ -8,6 +8,12 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("moretools")
@@ -18,12 +24,15 @@ public class More_Tools
     public static final String MOD_ID = "moretools";
 
     public More_Tools() {
+        final ModLoadingContext modLoadingContext = ModLoadingContext.get();
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         RegistryHandler.init();
 
         MinecraftForge.EVENT_BUS.register(this);
+        forgeEventBus.addListener(EventPriority.HIGH, oreGeneration::onBiomeLoading);
     }
 
     private void setup(final FMLCommonSetupEvent event) {}
