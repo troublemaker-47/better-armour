e82d451e0438152093bebcea06e125afe5b4cd82
troublemaker-47 <80205319+troublemaker-47@users.noreply.github.com>
2021-03-07 20:13:02 +0200

    Update More_Tools.java

--- src/main/java/com/tm_47/moretools/More_Tools.java
+++ src/main/java/com/tm_47/moretools/More_Tools.java
@@ -1,31 +1,30 @@
 package com.tm_47.moretools;
 
 import com.tm_47.moretools.util.RegistryHandler;
+import com.tm_47.moretools.world.gen.oreGeneration;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("moretools")
+@Mod(More_Tools.MOD_ID)
 public class More_Tools
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "moretools";
 
+
     public More_Tools() {
-        final ModLoadingContext modLoadingContext = ModLoadingContext.get();
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
